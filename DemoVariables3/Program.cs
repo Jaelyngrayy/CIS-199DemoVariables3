5c0f8341d07a881597946d258a10e3ae2519aff1
Jaelyn Gray <jjgray02@louisville.edu>
2023-04-06 22:35:24 -0400

    Add project files.

---
+++ DemoVariables3/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+class DemoVariables3
+{
+    static void Main()
+    {
+        int comparisonValue = 10;
+        bool isSixMore = 6 > comparisonValue;
+        WriteLine("When compairsonValue is {0}, isSixMore is {1}", comparisonValue, isSixMore);
+        comparisonValue = 2;
+        isSixMore = 6 > comparisonValue;
+        WriteLine("When compairsonValue is {0}, isSixMore is {1}", comparisonValue, isSixMore);
+
+    }
+}
